961819da2f65fcbe95c8406c692c35acb9ade706
harshithap123 <93457579+harshithap123@users.noreply.github.com>
2023-09-05 17:22:05 +0530

    Add files via upload

---
+++ src/app/components/Charging-components/Dashboard/card.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react'
+import { Box, useTheme } from '@mui/system'
+import { H3, Paragraph } from 'app/components/Typography'
+import { Grid, Card, IconButton, Icon } from '@mui/material'
+import jwt_decode from 'jwt-decode'
+import { axiosCharging } from '../../../../axios'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+const StatCard3 = () => {
+    const [count, setCount] = useState({})
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let user = localStorage.getItem('prexo-authentication')
+            if (user) {
+                let { user_name } = jwt_decode(user)
+                try {
+                    let res = await axiosCharging.post(
+                        '/dashboard/' + user_name
+                    )
+                    if (res.status === 200) {
+                        setCount(res.data.data)
+                    }
+                } catch (error) {
+                  
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: error,
+                    })
+                }
+            }
+        }
+        fetchData()
+    }, [])
+
+    const statList = [
+        {
+            icon: 'battery_charging_full',
+            amount: count.charging,
+            title: 'Charging Request',
+            path: '/charging/tray',
+        },
+    ]
+    const { palette } = useTheme()
+    const textMuted = palette.text.secondary
+
+    return (
+        <div>
+            <Grid container spacing={3}>
+                {statList.map((item, ind) => (
+                    <Grid key={item.title} item md={3} sm={6} xs={12}>
+                        <Card
+                            style={{ cursor: 'pointer' }}
+                            onClick={(e) => {
+                                navigate(item.path)
+                            }}
+                            elevation={3}
+                            sx={{ p: '20px', display: 'flex' }}
+                        >
+                            <div>
+                                <IconButton
+                                    size="small"
+                                    sx={{
+                                        padding: '8px',
+                                        background: 'rgba(0, 0, 0, 0.01)',
+                                    }}
+                                >
+                                    <Icon sx={{ color: textMuted }}>
+                                        {item.icon}
+                                    </Icon>
+                                </IconButton>
+                            </div>
+                            <Box ml={2}>
+                                <H3 sx={{ mt: '-4px', fontSize: '32px' }}>
+                                    {item.amount}
+                                </H3>
+                                <Paragraph sx={{ m: 0, color: textMuted }}>
+                                    {item.title}
+                                </Paragraph>
+                            </Box>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
+}
+
+export default StatCard3
